1ff4b0473c79150d5387ed2174b120128d465737
James Laverack <james@jameslaverack.com>
2015-12-24 18:34:02 +0000

    Add stub methods for expected paths

--- app.py
+++ app.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def hello():
     return "Hello, world!"
 
+@app.route("/user/<username>")
+def show_user(username):
+    return "User page for user " + username
+
+@app.route("/game/<gamename>")
+def show_game(gamename):
+    return "Game page for game " + gamename
+
+@app.route("/game/<gamename>/submit")
+def show_submit_score(gamename):
+    return "Submit a score for game " + gamename
+
+@app.route("/game/<gamename>/leaderboard")
+def show_leaderboard(gamename):
+    return "Show the leaderboard for game " + gamename
+
 if __name__ == "__main__":
     app.run();
