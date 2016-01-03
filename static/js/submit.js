3f0e2f146ddf781d2506b8d8f5abcc7c9c883a35
James Laverack <james@jameslaverack.com>
2016-01-03 14:50:15 +0000

    Return suggestions for player names

---
+++ static/js/submit.js
@@ -0,0 +1,15 @@
+var players_bloodhound = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: {
+	url: '/api/v1/player',
+	cache: false
+    }
+});
+
+players_bloodhound.initialize();
+
+$('.player-list').typeahead({
+    name: 'players',
+    source: players_bloodhound.ttAdapter()
+});
