06139d3c774b3f119d1ce364b7b1085fdc78fc21
James Laverack <james@jameslaverack.com>
2016-01-05 00:52:00 +0000

    Add `drop_db()` function

--- migrate.py
+++ migrate.py
@@ -1,7 +1,7 @@
 import db
 
 
-def create_tables():
+def init_db():
     conn = db.database_connection()
     cur = conn.cursor()
 
@@ -10,5 +10,21 @@ def create_tables():
 
     conn.commit()
 
+
+def drop_db():
+    conn = db.database_connection()
+    cur = conn.cursor()
+
+    table_names = ['game', 'player', 'score']
+
+    for table_name in table_names:
+        # Here we can't use parameters, but there's no risk of SQL
+        # injection as long as the table_names array is not
+        # user defined
+        cur.execute("DROP TABLE IF EXISTS " + table_name + " CASCADE")
+
+    conn.commit()
+
+
 if __name__ == "__main__":
-    create_tables()
+    init_db()
